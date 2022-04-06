43a7328ea4bc98546a9699dae511a9c635ddef30
Singroff <caleb.epps@yahoo.com>
2022-04-06 12:45:31 -0400

    Added Destiny API Functionality. Currently decodes hashes and can search for entities

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import asyncio
+import os.path
 import tkinter as tk
 from tkinter import *
 
 import pydest
+from os.path import exists
+import sqlite3
 from PIL import ImageTk, Image
 import requests
 import json
@@ -10,12 +13,20 @@ import json
 
 # Request Example
 async def main():
-    destiny = pydest.Pydest("941d92034e1b4563a6eefd80dc6786f8")
-    jsonTest = await destiny.api.search_destiny_entities("DestinyInventoryItemDefinition", "Guil")
-    newjson = await destiny.decode_hash(3844826443, "DestinyInventoryItemDefinition")
-    dP = newjson['displayProperties']
-    print(dP['name'])
-    await destiny.close()
+    if(os.path.exists('destinyDB.txt')):
+        print("\n\nDatabase Exists, continuing...\n\n")
+    else:
+        destiny = pydest.Pydest("941d92034e1b4563a6eefd80dc6786f8")
+        jsonTest = await destiny.api.get_destiny_manifest()
+        mani_url = 'http://www.bungie.net' + jsonTest['Response']['jsonWorldContentPaths']['en']['']
+        jsonDB = json.loads(requests.get(mani_url).text)
+        print(jsonDB)
+        print(mani_url)
+        with open('Test_FIles/destinyDB.txt', 'w') as f:
+            json.dump(jsonDB, f)
+
+
+
 
 
 loop = asyncio.get_event_loop()
