97fed584134f2aa6dee6035294c85fcd6997e0b6
Singroff <caleb.epps@yahoo.com>
2022-04-07 16:27:07 -0400

    Added Destiny API Functionality. Currently decodes hashes and can search for entities

--- main.py
+++ main.py
@@ -25,27 +25,40 @@ import json
 # Season Checklist
 # Lock by Class
 
+class Main:
+    async def main():
+        if (os.paths.exists("destinyDB.json")):
+           print("Database already exists")
+      else:
+           print("Database does not exist.")
 
-async def main():
-    if(os.path.exists('destinyDB.txt')):
-        print("\n\nDatabase Exists, continuing...\n\n")
-    else:
-        destiny = pydest.Pydest("941d92034e1b4563a6eefd80dc6786f8")
-        jsonTest = await destiny.api.get_destiny_manifest()
-        mani_url = 'http://www.bungie.net' + jsonTest['Response']['jsonWorldContentPaths']['en']['']
-        jsonDB = json.loads(requests.get(mani_url).text)
-        print(jsonDB)
-        print(mani_url)
-        with open('Test_FIles/destinyDB.txt', 'w') as f:
-            json.dump(jsonDB, f)
+# runGenerateDB
 
+# Build Dictionaries of each season at startup, save in encrypted files.
+# Access hashes via dictionary and pass them as params to decode_hash
+    async def generateDB():
 
 
+
+
+
+# async def main():
+#     if(os.path.exists('destinyDB.txt')):
+#         print("\n\nDatabase Exists, continuing...\n\n")
+#     else:
+#         destiny = pydest.Pydest("941d92034e1b4563a6eefd80dc6786f8")
+#         jsonTest = await destiny.api.get_destiny_manifest()
+#         mani_url = 'http://www.bungie.net' + jsonTest['Response']['jsonWorldContentPaths']['en']['']
+#         jsonDB = json.loads(requests.get(mani_url).text)
+#         print(jsonDB)
+#         print(mani_url)
+#         with open('Test_FIles/destinyDB.txt', 'w') as f:
+#             json.dump(jsonDB, f)
 
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
-#loop.close()
+# loop.close()
 
 
 # How to make window and display image
